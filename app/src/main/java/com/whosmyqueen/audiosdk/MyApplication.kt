1d41854f2f4f1e3c17beac3f0323961ac05da936
whosmyqueen <644049260@qq.com>
2022-08-05 16:36:36 +0800

    UPDATE: first commit

---
+++ app/src/main/java/com/whosmyqueen/audiosdk/MyApplication.kt
@@ -0,0 +1,12 @@
+package com.whosmyqueen.audiosdk
+
+import androidx.multidex.MultiDexApplication
+
+/**
+ * @Description TODO
+ * @Auther whosmyqueen
+ * @Date 2022/8/5 16:34
+ * @Version 1.0
+ */
+class MyApplication : MultiDexApplication() {
+}
